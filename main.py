d4455f78a7c051c50e36196b5a504c1af031ad7a
Edward J. Schwartz <edmcman@gmail.com>
2024-12-08 12:06:07 -0500

    password

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import network
 from time import sleep
 from uAPI import uAPI, HTTPResponse
 from picozero import pico_temp_sensor, pico_led
+from secret import ssid, password
 
 wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
 while wlan.isconnected() == False:
     print('Waiting for connection...')
     pico_led.blink()
