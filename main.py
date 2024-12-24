6eef5ebb713dc9a1d50299cbd8f45225b8e13e3f
Edward J. Schwartz <edmcman@gmail.com>
2024-12-23 20:37:50 -0500

    PM logging

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from uAPI import uAPI, HTTPResponse
 import os
 from picozero import pico_temp_sensor, pico_led
 
-ssid = 'ssid'
-password = 'password'
+ssid = 'verycreative-lr'
+password = '03141005'
 
-#pm=None for default power management
-pm=network.WLAN.PM_NONE
+pm=None #for default power management
+#pm=network.WLAN.PM_NONE
 
 async def connect_to_wifi():
     pico_led.off()
     wlan = network.WLAN(network.STA_IF)
     wlan.config(trace=1)
     if pm is not None: wlan.config(pm=pm)
-    print(f"Actual PM: {wlan.config('pm')}")
     wlan.active(True)
     wlan.connect(ssid, password)
     while not wlan.isconnected():
@@ -24,6 +23,7 @@ async def connect_to_wifi():
         print("Waiting for connection...")
         await uasyncio.sleep(1)
     print("Connected:", wlan.ifconfig())
+    print(f"Actual PM: {hex(wlan.config('pm'))}")
     pico_led.on()
     return wlan
 
@@ -33,9 +33,10 @@ async def monitor_wifi():
     c = 0
     while True:
         c += 1
-        if c % 1000 == 0:
+        if c % 100 == 0:
             print("Still checking Wi-Fi connection...")
             print(f"Debug: connected: {wlan.isconnected()} status: {wlan.status()}")
+            print(f"Actual PM: {hex(wlan.config('pm'))}")
         pico_led.off()
         await uasyncio.sleep(2)
         if not wlan.isconnected():
