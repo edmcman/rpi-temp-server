b311be203a8aa7b047b8cd1a86488cfa7ae1b4ec
Edward J. Schwartz <edmcman@cmu.edu>
2024-12-25 08:59:22 -0500

    Misc updates for experiments

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import uasyncio
 import network
-from time import sleep
 from uAPI import uAPI, HTTPResponse
 import os
 from picozero import pico_temp_sensor, pico_led
+import random
 
 ssid = 'verycreative-lr'
 password = '03141005'
 
-pm=eval(open("pm", "r").read().strip())
+pms = random.choice(["None", "0xa11140", "network.WLAN.PM_NONE", "network.WLAN.PM_PERFORMANCE", "network.WLAN.PM_POWERSAVE"])
+print(f"Random selected PM: {pms}")
+pm = eval(pms)
 
 async def connect_to_wifi():
     pico_led.off()
     wlan = network.WLAN(network.STA_IF)
     wlan.config(trace=1)
-    if pm is not None: wlan.config(pm=pm)
     wlan.active(True)
+    if pm is not None: wlan.config(pm=pm)
     wlan.connect(ssid, password)
     while not wlan.isconnected():
         pico_led.blink()
@@ -32,7 +34,7 @@ async def monitor_wifi():
     c = 0
     while True:
         c += 1
-        if c % 100 == 0:
+        if c % 20 == 0:
             print("Still checking Wi-Fi connection...")
             print(f"Debug: connected: {wlan.isconnected()} status: {wlan.status()}")
             print(f"Actual PM: {hex(wlan.config('pm'))}")
--- test.bash
+++ test.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-PM="${1?Please specify the pm setting}"
+#PM="${1?Please specify the pm setting}"
+MAX=3600
+
+set -e
 
 mpremote reset
-echo "Waiting for reset"
+echo "test.bash: Waiting for reset"
 sleep 10
-mpremote cp <(echo -n "$PM") :pm
+#mpremote cp <(echo -n "$PM") :pm
 (mpremote run main.py | tee mp.log)&
+
+set +e
 
 # kill mpremote on exit
 trap "kill -- -$$" EXIT
@@ -17,13 +22,16 @@ echo "test.bash: Waiting for the server to start"
 sleep 10
 
 n=1
-while true
+while [ $n -le $MAX ]
 do
   echo "test.bash: Sleeping for $n seconds"
   sleep $n
   date
   echo "test.bash: Running wget"
-  out=$(wget -q "$URL" -O /dev/stdout) || break
+  out=$(wget -q "$URL" -O /dev/stdout) || (echo "test.bash: Failed at $n seconds"; false) || exit 1
   echo "test.bash: Response: $out"
   n=$((n * 2))
 done
+
+echo "test.bash: Test succeeded"
+exit 0
